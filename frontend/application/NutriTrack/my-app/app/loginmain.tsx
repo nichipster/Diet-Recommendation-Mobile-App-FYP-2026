8c37eefd68ad394aa63fe4ec7eccac762d859a32
ophiuchi <hoojiasheng18@gmail.com>
2026-03-15 20:09:56 +0800

    delete application folder
    refer to jiasheng application folder for completed app

--- frontend/application/NutriTrack/my-app/app/loginmain.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Login1 from './components/first_section/login1';
-
-
-export default function Loginmain() {
-  return <Login1/>;
-}
--- frontend/application/Readme.txt
+++
@@ -1,2 +0,0 @@
-Currently login + signup is working along with the survey.
-Dashboard is working but meal logger isn't implemented yet.
