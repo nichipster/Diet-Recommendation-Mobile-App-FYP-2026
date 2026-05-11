2adb30a76c7b56538a1cc734a8d4b15e51fa3fef
L1b3r0-prog <leebpk@gmail.com>
2026-05-11 13:26:30 +0800

    Update CreateSchedule.tsx

--- frontend/NutriTrack/my-app/components/nutritionist_section/CreateSchedule.tsx
+++ frontend/NutriTrack/my-app/components/nutritionist_section/CreateSchedule.tsx
@@ -24,7 +24,7 @@ const MONTHS = [
   "January","February","March","April","May","June",
   "July","August","September","October","November","December",
 ];
-const ALL_TIMES = ["09:00","10:00","11:00","13:00","14:00","15:00","16:00","17:00"];
+const ALL_TIMES = ["08:00", "09:00","10:00","11:00","13:00","14:00","15:00","16:00","17:00"];
 
 const TODAY = new Date();
 
